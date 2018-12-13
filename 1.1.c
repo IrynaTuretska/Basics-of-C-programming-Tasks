abbead95ab3d276239630fb97f8568848399df04
chicagoist <le.legioner@gmail.com>
2018-12-13 14:21:00 +0200

    Add first task 1.1c

---
+++ 1.1.c
@@ -0,0 +1,29 @@
+/*
+Напишите программу, которая выводит на экран следующие строки:
+
+Hello, World!
+The world is yours!
+
+ Sample Output:
+
+Hello, World!
+The world is yours!
+*/
+
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello, World!\n");
+  printf("The world is yours!");
+  return 0;
+}
+
+/*
+#include <stdio.h>
+
+int main() {
+  puts("Hello, World!");
+  puts("The world is yours!");
+  return 0;
+}
+*/
