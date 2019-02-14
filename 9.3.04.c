884ca443a2c1d27738521adbe7eed99be6c0ac0a
chicagoist <le.legioner@gmail.com>
2019-02-14 20:07:35 +0200

    New Tasks

--- 9.3.04.c
+++ 9.3.04.c
@@ -38,7 +38,7 @@ int main()
     fflush(stdin);
     fgets(str2, 50, stdin);
 
-    for(i = 0, j = 0; i < strlen(str1), j < strlen(str2); ++i, ++j)
+    for(i = 0, j = 0; i < strlen(str1) && j < strlen(str2); ++i, ++j)
         if(str1[i] != str2[j]){
             printf("no");
             return 0;
