59c0d32b1201630bb5d926f7b3e34afd6f19da06
chicagoist <le.legioner@gmail.com>
2019-02-21 17:29:45 +0200

    New Tasks

---
+++ 10.3.01.c
@@ -0,0 +1,33 @@
+/*
+10.3 Аргументы функций
+  
+
+Написать функцию void swap(int * x, int * y), которая меняет значения переменных местами.
+
+Sample Input:
+
+2 0
+
+Sample Output:
+
+0 2
+
+*/
+
+void swap(int * a, int * b){
+    int tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+
+}
+
+/*
+ * better code
+ *
+ void swap(int * a, int * b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+*/
---
+++ 10.3.02.c
@@ -0,0 +1,55 @@
+/*
+10.3 Аргументы функций
+  
+
+Написать функцию void sum_digits(int * x), которая заменяет число x на сумму цифр из которых состоит число x.
+
+Sample Input 1:
+
+12938
+
+Sample Output 1:
+
+23
+
+Sample Input 2:
+
+722
+
+Sample Output 2:
+
+11
+
+Sample Input 3:
+
+1
+
+Sample Output 3:
+
+1
+
+*/
+
+void sum_digits(int * x){
+    int sum = 0;
+
+    for(int i = *x; i > 0; i = (i/10)){
+        sum = sum + i%10;
+
+    }
+
+    *x = sum;
+}
+
+/*
+ * better code
+ *
+void sum_digits(int * x){
+    int s = 0;
+    while (*x != 0) {
+        s += *x % 10; *x /= 10;
+    }
+    *x = s;
+}
+
+*/
