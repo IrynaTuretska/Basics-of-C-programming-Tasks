d66764d1cc1227f592b1fb479ecba3ef7944bd2f
chicagoist <le.legioner@gmail.com>
2019-03-22 18:51:04 +0200

    sscanf

--- 09.3.Стандартные_функции_работы_со_строками_sscanf/09.3.04.c
+++ 09.3.Стандартные_функции_работы_со_строками_sscanf/09.3.04.c
@@ -1,28 +1,28 @@
 /*
-9.3 Стандартные функции работы со строками.
+   9.3 Стандартные функции работы со строками.
 
 
-Сравнение строк
-Даны две строки. Необходимо сравнить строки между собой с учётом регистра символов.
+   Сравнение строк
+   Даны две строки. Необходимо сравнить строки между собой с учётом регистра символов.
 
-Входные данные:
-Две символьных строки, записанные каждая с новой строки. Длина строк не более 50 символов.
+   Входные данные:
+   Две символьных строки, записанные каждая с новой строки. Длина строк не более 50 символов.
 
-Выходные данные:
-yes если строки полностью совпадают, no в противном случае.
+   Выходные данные:
+   yes если строки полностью совпадают, no в противном случае.
 
-Sample Input:
+   Sample Input:
 
-stepik
-Stepik
+   stepik
+   Stepik
 
-Sample Output:
+   Sample Output:
 
-no
+   no
 
 
 
-*/
+ */
 
 #include <stdio.h>
 #include <string.h>
@@ -30,33 +30,33 @@ no
 
 int main()
 {
-    char str1[50];
-    char str2[50];
-    int i, j;
-    
-    fgets(str1,50, stdin);
-    fflush(stdin);
-    fgets(str2, 50, stdin);
+        char str1[50];
+        char str2[50];
+        int i, j;
 
-    for(i = 0, j = 0; i < strlen(str1) && j < strlen(str2); ++i, ++j)
-        if(str1[i] != str2[j]){
-            printf("no");
-            return 0;
-      }
-    printf("yes"); 
-          
-    return 0;
+        fgets(str1,50, stdin);
+        fflush(stdin);
+        fgets(str2, 50, stdin);
+
+        for(i = 0, j = 0; i < strlen(str1) && j < strlen(str2); ++i, ++j)
+                if(str1[i] != str2[j]) {
+                        printf("no");
+                        return 0;
+                }
+        printf("yes");
+
+        return 0;
 }
 
 /*
  * better code
  *
-#include <stdio.h>
-#include <string.h>
+ #include <stdio.h>
+ #include <string.h>
 
-int main() {
+   int main() {
     char s[100], s1[100];
     gets(s); fflush(stdin); gets(s1);
     printf(strcmp(s, s1) ? "no" : "yes");
-}
-*/
+   }
+ */
--- 09.3.Стандартные_функции_работы_со_строками_sscanf/09.3.05.c
+++ 09.3.Стандартные_функции_работы_со_строками_sscanf/09.3.05.c
@@ -1,90 +1,90 @@
 /*
 
-9.3 Стандартные функции работы со строками.
-6 из 10 шагов пройдено
-5 из 9 баллов  получено
-Вы прошли больше 80% курса, оставьте отзыв
-  
+   9.3 Стандартные функции работы со строками.
+   6 из 10 шагов пройдено
+   5 из 9 баллов  получено
+   Вы прошли больше 80% курса, оставьте отзыв
 
-Сравнение строк
-Даны две строки. Необходимо сравнить строки между собой без учёта регистра символов.
 
-Входные данные:
-Две символьных строки, записанные каждая с новой строки. Длина строк не более 50 символов.
+   Сравнение строк
+   Даны две строки. Необходимо сравнить строки между собой без учёта регистра символов.
 
-Выходные данные:
-yes если строки полностью совпадают, no в противном случае.
+   Входные данные:
+   Две символьных строки, записанные каждая с новой строки. Длина строк не более 50 символов.
 
-Sample Input:
+   Выходные данные:
+   yes если строки полностью совпадают, no в противном случае.
 
-Hello WorlD
-hello world
+   Sample Input:
 
-Sample Output:
+   Hello WorlD
+   hello world
 
-yes
+   Sample Output:
 
-*/
+   yes
+
+ */
 #include <stdio.h>
 #include <string.h>
 
 
 int main()
 {
-    char str1[50];
-    char str2[50];
-    int i, j, a;
-    
-    fgets(str1,50, stdin);
-    fflush(stdin);
-    fgets(str2, 50, stdin);
+        char str1[50];
+        char str2[50];
+        int i, j, a;
 
-     while(strlen(str1) == strlen(str2)){
+        fgets(str1,50, stdin);
+        fflush(stdin);
+        fgets(str2, 50, stdin);
 
-       for(a = 0; a < strlen(str1); ++a){
+        while(strlen(str1) == strlen(str2)) {
+
+                for(a = 0; a < strlen(str1); ++a) {
 
 
-         if((str1[a] == 32 && str2[a] != 32) || (str2[a] == 32 && str1[a] != 32))
-         {
-           printf("no");  
-              return 0;
-         }
-           
-       }
+                        if((str1[a] == 32 && str2[a] != 32) || (str2[a] == 32 && str1[a] != 32))
+                        {
+                                printf("no");
+                                return 0;
+                        }
 
-    for(i = 0, j = 0 ; i < strlen(str1) && j < strlen(str2); ++i, ++j)
-    
-     {
+                }
 
-       if(str1[i] != str2[j] && str1[i] != str2[j]+32 && str1[i] !=32)
-       if(str2[i] != str1[j] && str2[i] != str1[j]+32 && str2[i] !=32)
-       {
-         printf("no");
-            return 0;
-            
-      }
-      continue;
-      
-    }
-    
-    printf("yes");
-    return 0; 
+                for(i = 0, j = 0; i < strlen(str1) && j < strlen(str2); ++i, ++j)
 
-     }
+                {
 
-   printf("no");  
-          
-    return 0;
+                        if(str1[i] != str2[j] && str1[i] != str2[j]+32 && str1[i] !=32)
+                                if(str2[i] != str1[j] && str2[i] != str1[j]+32 && str2[i] !=32)
+                                {
+                                        printf("no");
+                                        return 0;
+
+                                }
+                        continue;
+
+                }
+
+                printf("yes");
+                return 0;
+
+        }
+
+        printf("no");
+
+        return 0;
 }
 
 
 /*
  * better code
  *
-#include <stdio.h>
-#include <string.h>
+ #include <stdio.h>
+ #include <string.h>
 
-int main() {
+   int main() {
     char s[50], s1[50];
     gets(s); fflush(stdin); gets(s1);
     for (int i = 0; i < strlen(s); i++) {
@@ -94,5 +94,5 @@ int main() {
         s1[i] += s1[i] < 'a' ? 32 : 0;
     }
     printf(strcmp(s, s1) ? "no" : "yes");
-}
-*/
+   }
+ */
